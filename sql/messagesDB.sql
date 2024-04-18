bb9df18f5ead76a02a01606df4b8e435474ab972
Pau <paumartin@opendeusto.es>
2024-04-18 13:08:54 +0200

    Update messagesDB.sql
    He quitado las comillas del root

--- sql/messagesDB.sql
+++ sql/messagesDB.sql
@@ -5,10 +5,10 @@ DROP DATABASE IF EXISTS messagesDB;
 CREATE DATABASE messagesDB;
 
 /* CREAR un NUEVO USUARIO (si pretendías crear 'spq' en lugar de usar 'root') */
-CREATE USER IF NOT EXISTS 'root´'@'localhost' IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
 
 /*  TODOS LOS PRIVILEGIOS sobre 'messagesDB' AL NUEVO USUARIO */
-GRANT ALL PRIVILEGES ON messagesDB.* TO 'root´'@'localhost';
+GRANT ALL PRIVILEGES ON messagesDB.* TO 'root'@'localhost';
 
 FLUSH PRIVILEGES;
 
