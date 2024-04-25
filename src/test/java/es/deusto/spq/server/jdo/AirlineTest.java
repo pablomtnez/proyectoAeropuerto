f66c8654d2ab50fe1f1cbbaf82cdf9d7af78cff4
miguelSolana <81014384+MiguelSolana@users.noreply.github.com>
2024-04-25 10:48:00 +0200

    Primer test y carpetas test

---
+++ src/test/java/es/deusto/spq/server/jdo/AirlineTest.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AirlineTest {
+
+    private Airline airline;
+
+    @Before
+    public void setUp() {
+        // Create mock objects for dependencies
+        Country countryMock = mock(Country.class);
+        AirAlliance allianceMock = mock(AirAlliance.class);
+        List<Flight> flightsMock = new ArrayList<>();
+
+        // Create instance of Airline with mocked dependencies
+        airline = new Airline("code", "name", countryMock, allianceMock, flightsMock);
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals("code", airline.getCode());
+        assertEquals("name", airline.getName());
+        assertEquals(countryMock, airline.getPais());
+        assertEquals(allianceMock, airline.getAlliance());
+        assertEquals(flightsMock, airline.getVuelos());
+    }
+
+    @Test
+    public void testSetters() {
+        airline.setCode("newCode");
+        assertEquals("newCode", airline.getCode());
+        
+        airline.setName("newName");
+        assertEquals("newName", airline.getName());
+        
+        Country newCountry = mock(Country.class);
+        airline.setPais(newCountry);
+        assertEquals(newCountry, airline.getPais());
+        
+        AirAlliance newAlliance = mock(AirAlliance.class);
+        airline.setAlliance(newAlliance);
+        assertEquals(newAlliance, airline.getAlliance());
+        
+        List<Flight> newFlights = new ArrayList<>();
+        airline.setVuelo(newFlights);
+        assertEquals(newFlights, airline.getVuelos());
+    }
+
+    @Test
+    public void testToString() {
+        String expectedToString = "code: name [null] (null)";
+        assertEquals(expectedToString, airline.toString());
+    }
+}
