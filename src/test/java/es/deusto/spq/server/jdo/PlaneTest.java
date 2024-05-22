45047dc0a78f1377985ac69f7333c8928cbd9909
corxbil-2 <115790287+corxbil-2@users.noreply.github.com>
2024-05-22 13:41:44 +0200

    Plane CSV Test
    Test para testParseCSV creado en JDO de plane

--- src/test/java/es/deusto/spq/server/jdo/PlaneTest.java
+++ src/test/java/es/deusto/spq/server/jdo/PlaneTest.java
@@ -1,6 +1,10 @@
 package es.deusto.spq.server.jdo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -38,4 +42,30 @@ public class PlaneTest {
         String expectedToString = "code: name (200 seats)";
         assertEquals(expectedToString, plane.toString());
     }
+
+    @Test
+    public void testParseCSV_ValidInput() {
+        String csvData = "737,Boeing 737,189";
+        try {
+            Plane plane = Plane.parseCSV(csvData);
+            assertNotNull(plane);
+            assertEquals("737", plane.getIataCode());
+            assertEquals("Boeing 737", plane.getName());
+            assertEquals(189, plane.getSeats());
+        } catch (Exception e) {
+            fail("Exception should not be thrown for valid input");
+        }
+    }
+
+    @Test
+    public void testParseCSV_InvalidInput() {
+        String csvData = "737,Boeing 737,invalid_seats";
+        Exception exception = assertThrows(Exception.class, () -> {
+            Plane.parseCSV(csvData);
+        });
+        String expectedMessage = "class es.deusto.spq.server.jdo.Plane from CSV error: " + csvData;
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
