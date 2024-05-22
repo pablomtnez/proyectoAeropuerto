9427e3669ebd7d49e3f79c300f9c82d5539efdc8
corxbil-2 <115790287+corxbil-2@users.noreply.github.com>
2024-05-22 13:34:01 +0200

    Airline CSV Test
    Se ha creado un test para la funcion testParseCSV

--- src/test/java/es/deusto/spq/server/jdo/AirlineTest.java
+++ src/test/java/es/deusto/spq/server/jdo/AirlineTest.java
@@ -1,6 +1,10 @@
 package es.deusto.spq.server.jdo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
@@ -57,4 +61,31 @@ public class AirlineTest {
     String expectedToString = String.format("%s: %s [%s] (%s)", "code", "name", allianceMock, countryMock);
     assertEquals(expectedToString, airline.toString());
     }
+
+    @Test
+    public void testParseCSV_ValidInput() {
+        String csvData = "AA,American Airlines,US,STAR_ALLIANCE";
+        try {
+            Airline airline = Airline.parseCSV(csvData);
+            assertNotNull(airline);
+            assertEquals("AA", airline.getIataCode());
+            assertEquals("American Airlines", airline.getName());
+            assertEquals(Country.US, airline.getCountry());
+            assertEquals(AirAlliance.STAR_ALLIANCE, airline.getAlliance());
+        } catch (Exception e) {
+            fail("Exception should not be thrown for valid input");
+        }
+    }
+
+    @Test
+    public void testParseCSV_InvalidInput() {
+        String csvData = "AA,American Airlines,UNKNOWN_COUNTRY,ONEWORLD";
+        Exception exception = assertThrows(Exception.class, () -> {
+            Airline.parseCSV(csvData);
+        });
+        String expectedMessage = "class es.deusto.spq.server.jdo.Airline from CSV error: " + csvData;
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    
 }
